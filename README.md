9a0f47b525e8e12632e2db3213c76a886939b2bd
渠猛 <qumeng@51yjtc.com>
2020-06-08 20:26:41 +0800

    tihuan

--- README.md
+++
@@ -1,2 +0,0 @@
-# testjenkins
-测试jekins
---
+++ src/main/java/com/example/jenkinsboot/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.jenkinsboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Copyright (C), 2019, 弗恩天下
+ * @Author: 渠猛
+ * @Date: 2020/6/8 0008 下午 8:01
+ * @Description:
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    private String test(){
+        return "hello world";
+    }
+
+}
