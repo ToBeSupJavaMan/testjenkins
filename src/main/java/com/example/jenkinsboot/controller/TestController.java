3b3465a2cbeb5b0dbdfa611f996c6bd4d65e5dc8
ToBeSupJavaMan <31299928+ToBeSupJavaMan@users.noreply.github.com>
2020-06-12 15:59:36 +0800

    Update TestController.java

--- src/main/java/com/example/jenkinsboot/controller/TestController.java
+++ src/main/java/com/example/jenkinsboot/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @RequestMapping("/hello")
     @ResponseBody
     private String test(){
-        return "hello world3";
+        return "hello world4";
     }
 
 }
